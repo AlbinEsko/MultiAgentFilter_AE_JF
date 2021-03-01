2c98bcb5f212a309bcfc2a800fa01178dcb025cf
AlbinEsko <47393000+AlbinEsko@users.noreply.github.com>
2021-03-01 13:11:07 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 13:08:34 2021
+
+@author: golf_
+"""
+inputs = (
+        ("test1", "label")
+        )
+
+def perform(level, box, options):
+    print("Armed and ready")
+    
