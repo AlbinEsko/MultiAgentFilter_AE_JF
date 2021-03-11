c6c46fe16d5928f30a4df4648f099c3678030bfa
Johan Fritiofsson <47355404+master-johan@users.noreply.github.com>
2021-03-11 15:05:13 +0100

    Createhightmap added

--- MAF_main.py
+++ MAF_main.py
@@ -4,10 +4,38 @@ Created on Mon Mar  1 13:08:34 2021
 
 @author: Albin Esko & Johan Fritiofsson
 """
+from array import *
+from pymclevel import alphaMaterials
+
+from typing import List, Any
+
 inputs = (
-        ("test1", "label"),
-        )
+    ("test1", "label"),
+)
+
 
 def perform(level, box, options):
     print("Armed and ready")
-    
+    createHeightMap(level, box)
+
+
+def createHeightMap(level, box):
+    row = box.maxx - box.minx #vågräta
+    column = box.maxz - box.minz #lodräta
+    heightmap = [[0 for i in range(row)] for j in range(column)]
+
+    for r in heightmap:
+        print(r)
+
+    for z in range(column):  # range(box.minx, box.maxx, 1):
+        for x in range(row):  # range(box.minz, box.maxz, 1):
+            for y in range(box.maxy, box.miny, -1):
+                if (level.blockAt(box.minx+x, y, box.minz+z)) != 0:
+                    heightmap[z][x] = y
+
+                    break
+
+    for r in heightmap:
+        print(r)
+
+    return heightmap
